1825b3bf5b06cf4d0daeefcfe2f4a0d80c3ffc8e
Satish9047 <satishprajapati930@gmail.com>
2023-08-04 09:10:20 +0545

    1st commit added login and register functionality in the backend api

---
+++ server/controller/userAuth.js
@@ -0,0 +1,103 @@
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const User = require("../model/userModel");
+const saltRound = 10;
+const secret = `${process.env.SECRET_JWT}`;
+
+//register
+const registerController = async (req, res) => {
+  console.log(req.body);
+  const { email, contact, password } = req.body;
+
+  //validation
+  if (!email || !validator.isEmail(email)) {
+    return res.status(400).json({ error: "invalid email" });
+  }
+
+  if (!contact || !validator.isNumeric(contact)) {
+    return res.status(400).json({ error: "invalid contact" });
+  }
+
+  if (!password || validator.isLength(password, { min: 8 })) {
+    return res.status(400).json({ error: "invalid password" });
+  }
+
+  //hashing and saving
+  try {
+    bcrypt.hash(password, saltRound, async (error, hash) => {
+      if (error) {
+        console.log("couldn't create hashed password :", error);
+        return res.status(500).json({ error: "internal server error" });
+      }
+
+      const newUser = new User({
+        email: email,
+        contact: contact,
+        password: hash,
+      });
+      await newUser.save();
+    });
+
+    res.status(200).json({ message: "User register Successfully" });
+  } catch (error) {
+    console.log("can't register new User", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+//login
+const loginController = async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  //validation
+  if (!email || !validator.isEmail(email)) {
+    return res.status(400).json({ error: "Invalid email" });
+  }
+
+  if (!password || !validator.isLength(password, { min: 8 })) {
+    return res.status(400).json({ error: "Invalid password" });
+  }
+
+  //calidating password and generating jwt
+  try {
+    //checking if user is available
+    const existUser = await User.findOne({ email: String(email) });
+    if (!existUser) {
+      return res.status(400).json({ error: "invalid username" });
+    }
+
+    //comparing the existing user's password
+    bcrypt.compare(password, existUser.password, (error, result) => {
+      if (error|| !result) {
+        return res.status(400).json({ error: "invalid password" });
+      }
+    });
+
+    const token = jwt.sign(
+      { email: existUser.email },
+      secret,
+      { expiresIn: "5h" },
+      (error, token) => {
+        if (error) {
+          console.error(error);
+          return res.status(500).json({ error: "internal server error" });
+        }
+
+        console.log(token);
+      }
+    );
+
+    res.status(200).json({ success: "login successfull", jwtToken: token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "internal server error" });
+  }
+};
+
+//exports
+module.exports = {
+  loginController,
+  registerController,
+};
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose =  require("mongoose");
+const router = require("./router/router");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(`${MONGODB_URL}`)
+    .then(()=>{
+        console.log("MongoDB connected Successfully")
+    })
+    .catch((error)=>{
+        console.log("Can't conneect to the MongoDB")
+    });
+
+
+app.use("/", router);
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port: ${PORT}`);
+})
